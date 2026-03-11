e87a635189db445b458830c5f1656c38098bd146
riejan13 <reejan1010@gmail.com>
2026-03-11 18:34:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-function addTask(){
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let input = document.getElementById("taskInput");
-let task = input.value;
+function displayTasks(){
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
 
-if(task === "") return;
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+        li.textContent = task;
 
-let li = document.createElement("li");
-li.textContent = task;
+        li.onclick = function(){
+            tasks.splice(index,1);
+            saveTasks();
+        };
 
-li.onclick = function(){
-li.remove();
-};
-
-document.getElementById("taskList").appendChild(li);
-
-input.value = "";
+        list.appendChild(li);
+    });
 }
+
+function addTask(){
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if(task === "") return;
+
+    tasks.push(task);
+    input.value = "";
+
+    saveTasks();
+}
+
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+}
+
+displayTasks();
